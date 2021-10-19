202cc386fcbd10c160ddf4067916f5abb248832a
RolandWarburton <warburtonroland@gmail.com>
2021-10-19 18:41:15 +1100

    Small update runMe.sj

--- runMe.sh
+++ runMe.sh
@@ -38,30 +38,30 @@ echo "linking production.yaml" &
 ln -s "$root/docker/production.yaml" ../production.yaml 2>/dev/null
 echo "linking .dockerignore" &
 ln -s "$root/docker/.dockerignore" ../.dockerignore 2>/dev/null
-echo "done ================================================"
+echo "[COMPLETE]\n"
 
-sleep 1
+sleep 0.25
 
-echo "Creating blog containers"
+echo "[STEP]\t Creating blog containers"
 /usr/bin/docker volume create --name=blog_content
 /usr/bin/docker volume create --name=blogwatcher_database
 /usr/bin/docker volume create --name=blogwatcher_dev_node_modules
 /usr/bin/docker volume create --name=management_ui_dev_node_modules
 /usr/bin/docker volume create --name=blog_nginx_proxy_certs
-echo "done ================================================"
+echo "[COMPLETE]\n"
 
-sleep 1
+sleep 0.25
 
-echo "Moving static content to blog_content volume"
+echo "[STEP]\t Moving static content to blog_content volume"
 # Make dist directory inside the blog_content volume
-/usr/bin/docker run --rm --name temp_container -v blog_content:/html alpine mkdir /html/static
+/usr/bin/docker run --rm --name temp_container -v blog_content:/html alpine mkdir -p /html/static
 
 # Move placeholder index.html to dist
 /usr/bin/docker create --name temp_container -v blog_content:/html alpine >/dev/null
 if [ $BLOG_ENV = "development" ]; then
     /usr/bin/docker cp ./public/html/landing_development/index.html temp_container:/html
 else
-    /usr/bin/docker cp ./public/html/html/landing_production/index.html temp_container:/html
+    /usr/bin/docker cp ./public/html/landing_production/index.html temp_container:/html
 fi
 /usr/bin/docker rm temp_container >/dev/null
 
@@ -72,7 +72,6 @@ fi
 
 # Copy styles to root
 /usr/bin/docker create --name temp_container -v blog_content:/html alpine >/dev/null
-/usr/bin/docker cp ./public/css/home.css temp_container:/html/
 /usr/bin/docker cp ./public/css/menu.css temp_container:/html/
 /usr/bin/docker cp ./public/css/solarized.css temp_container:/html/
 /usr/bin/docker cp ./public/css/tiny_dark.css temp_container:/html/
@@ -91,11 +90,11 @@ fi
 /usr/bin/docker cp ./public/media/twitter.svg temp_container:/html/
 /usr/bin/docker cp ./public/media/linkedin.svg temp_container:/html/
 /usr/bin/docker rm temp_container >/dev/null
-echo "done ================================================"
+echo "[COMPLETE]\n"
 
-sleep 1
+sleep 0.25
 
-echo "Fixing file ownership in blog_content volume"
+echo "[STEP]\t Fixing file ownership in blog_content volume"
 # Fix ownership issues from copying files in and stuff
 # fix ownership from 1000:1000 to root:root
 /usr/bin/docker run --rm --name temp_container -v blog_content:/html alpine chown -R root:root /html/
@@ -103,11 +102,11 @@ echo "Fixing file ownership in blog_content volume"
 /usr/bin/docker run --rm --name temp_container -v blog_content:/html alpine find /html -type f -exec chmod 777 {} \;
 # change to -rwxrwxrwx for all directories
 /usr/bin/docker run --rm --name temp_container -v blog_content:/html alpine find /html -type d -exec chmod 777 {} \;
-echo "done ================================================"
+echo "[COMPLETE]\n"
 
-sleep 1
+sleep 0.25
 
-echo "Moving certificates to blog_nginx_proxy_certs volume"
+echo "[STEP]\t Moving certificates to blog_nginx_proxy_certs volume"
 # the cert folder could container
 # cert.pem
 # chain.pem
@@ -168,15 +167,21 @@ for d in $FILES; do
     fi
 
 done
-echo "done ================================================"
+echo "[COMPLETE]\n"
 
-sleep 1
+sleep 0.25
 
-echo "Creating gateway_network"
-/usr/bin/docker network create gateway_network
-echo "done ================================================"
+echo "[STEP]\t Creating gateway_network"
 
-sleep 1
+# if the gateway_network does not exist then create it
+if ! /usr/bin/docker network inspect gateway_network >/dev/null 2>&1; then
+    echo "Creating gateway_network"
+    /usr/bin/docker network create gateway_network
+fi
+
+echo "[COMPLETE]\n"
+
+sleep 0.25
 
 # echo "Moving nginx configs to blog_nginx_sites_enabled volume"
 # /usr/bin/docker create --name temp_container -v blog_nginx_sites_enabled:/data alpine
@@ -186,9 +191,9 @@ sleep 1
 #     /usr/bin/docker cp ../nginxProxy/production/sites-available/. temp_container:/data
 # fi
 # /usr/bin/docker rm temp_container
-# echo "done ================================================"
+# echo "[COMPLETE]\n"
 
-# sleep 1
+# sleep 0.25
 
 # echo "Building management UI and installing it to blog_content volume"
 # cd ../managementUI/
@@ -205,9 +210,9 @@ sleep 1
 # -v blog_content:/html management_ui cp -r /usr/src/app/build/. /html/admin
 
 # /usr/bin/docker rm management_ui_build_container
-# echo "done ================================================"
+# echo "[COMPLETE]\n"
 
-# sleep 1
+# sleep 0.25
 
 # ! Depricated because the blog should not handle non TLD requests
 # move non blog index pages to blog_content volume
